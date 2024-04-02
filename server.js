ec7a6e44e7ecd46abc8804cdb13a2dca09b0e942
arttuheinila <kauppa@tuta.io>
2024-04-02 15:41:26 +0300

    add authorization

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
+const todoRoutes = require('./routes/todoRoutes');
 
 const app = express();
 // Middleware for parsing JSON bodies
@@ -7,6 +8,7 @@ app.use(express.json());
 
 // Use routes
 app.use('/api/users', userRoutes);
+app.use('/api/todos', todoRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
